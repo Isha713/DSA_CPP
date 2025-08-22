92220a3d20b5867fc29ca4618fb5938891f4b58c
Isha713 <ishasaini713@gmail.com>
2025-08-22 21:17:39 +0530

    add new file

---
+++ Patterns/inverted_pyramid.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+    for (int i = n; i >= 1; i--) {
+        for (int space = 0; space < n - i; space++) {
+            cout << "  ";
+        }
+        for (int j = 0; j < 2*i - 1; j++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
