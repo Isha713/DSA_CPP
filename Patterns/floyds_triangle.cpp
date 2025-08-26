5b3e4df6323450ead654dfdb8ee2548f50b0b445
Isha713 <ishasaini713@gmail.com>
2025-08-26 09:21:38 +0530

    add new file

---
+++ Patterns/floyds_triangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int rows;
+    cout<<"Enter the numner of rows : ";
+    cin>>rows;
+    int n=1;
+
+    for(int i=1;i<=rows;i++){
+        for(int j =1;j<=i;j++){
+            cout<<n<<" ";
+            ++n;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
