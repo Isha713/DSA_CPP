e83b22676efe71b99362d066d33d5c2f40bdc1c8
Isha713 <ishasaini713@gmail.com>
2025-09-10 06:23:58 +0530

    add new file

---
+++ Functions/rev_num.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int reverseNumber(int n){
+    int rev = 0;
+    while(n > 0) {
+        rev = rev*10 + n%10;
+        n/=10;
+    }
+    return rev;
+}
+int main(){
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    cout<<"Reversed = " << reverseNumber(num);
+    return 0;
+}
