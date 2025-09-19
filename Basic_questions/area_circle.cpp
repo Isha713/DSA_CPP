b20b48158dabca5846d949b7cad48bf0a9ba5b9e
Isha713 <ishasaini713@gmail.com>
2025-09-19 20:33:24 +0530

    add new file

---
+++ Basic_questions/area_circle.cpp
@@ -0,0 +1,10 @@
+# include<iostream>
+using namespace std;
+int main(){
+    int radius;
+    cout<<"Enter the radius of circle : ";
+    cin>>radius;
+    int area = 3.14*radius*radius;
+    cout<<"The area of circle is : "<<area;
+    return 0;
+}
