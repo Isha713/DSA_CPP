86d8a74617f7089171de7d2998e3e7da5e5caff4
Isha713 <ishasaini713@gmail.com>
2025-09-19 20:35:09 +0530

    add new file

---
+++ Basic_questions/C_to_F.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int C;
+    cout<<"Enter the temperature in Celsius : ";
+    cin>>C;
+    int F=C*9/5+32;
+    cout<<"The temperature in Fahrenheit becomes : "<<F;
+    return 0;
+}
