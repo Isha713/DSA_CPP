9e02f1ee99d9ddc9367bc0d0b10047ce0d524e6e
Isha713 <ishasaini713@gmail.com>
2026-02-11 18:29:12 +0530

    Add new file

---
+++ Array_2D/max_row_sum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int getMaxSum(int arr[][3],int rows,int cols){
+    int maxRowSum=INT_MIN;
+    for(int i=0;i<rows;i++){
+        int rowSumI=0;
+        for(int j=0;j<cols;j++){
+            rowSumI+=arr[i][j];
+        }
+        maxRowSum = max(maxRowSum,rowSumI);
+    }
+    return maxRowSum;
+}
+int main(){
+    int array[3][3]={{1,2,3} , {4,5,6} ,{7,8,9}};
+    int rows=3;
+    int cols=3;
+    cout<<getMaxSum(array,rows,cols)<<endl;
+    return 0;
+}
